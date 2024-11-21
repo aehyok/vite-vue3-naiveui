97b717fde0a837756855203913cd73919d97c647
刘启明 <455043818@qq.com>
2024-11-21 19:27:30 +0800

    feat: 引入naiveui

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <n-button type="primary" @click="() => { count = count + 1 }">{{ count }}</n-button>
 </template>
+<script setup lang="ts">
+import { NButton } from 'naive-ui'
+import { ref } from 'vue'; 
+
+const count = ref(0)
+</script>
 
 <style scoped>
 .logo {
